76cff7bbebd9675e8c0acbf8e5a1140a40355699
Alex N <ichiv.0zv@gmail.com>
2022-12-13 06:52:01 +0800

    Control added

--- Assets/Scripts/Deliver.cs
+++ Assets/Scripts/Deliver.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Deliver : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 10;
+    [SerializeField] float steerSpeed = 10;
     // Start is called before the first frame update
     void Start()
     {
-        transform.Rotate(0,0,30);
+        //transform.Rotate(0,0,30);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0,0,-30*Time.deltaTime);
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed*Time.deltaTime;
+        float speedAmount = Input.GetAxis("Vertical") * moveSpeed*Time.deltaTime;
+        if(speedAmount < 0)
+        {
+            steerAmount = steerAmount* -1;
+
+        }
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, speedAmount, 0);
     }
 }
